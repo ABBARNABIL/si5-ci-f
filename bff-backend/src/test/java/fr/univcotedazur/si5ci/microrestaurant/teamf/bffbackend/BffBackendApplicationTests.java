24c43c98ba1bbfad97ad68f7915f9bd757a03c10
Lidaou Denis Assoti <denisassoti@yahoo.com>
2022-09-19 14:27:28 +0200

    create bff-backend spring project and .gitignore

---
+++ bff-backend/src/test/java/fr/univcotedazur/si5ci/microrestaurant/teamf/bffbackend/BffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univcotedazur.si5ci.microrestaurant.teamf.bffbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
