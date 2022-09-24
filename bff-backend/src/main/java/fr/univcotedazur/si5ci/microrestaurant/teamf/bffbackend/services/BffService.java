0ce8d036625815f1923fd53c40df10f676b4f47a
Lidaou Denis Assoti <denisassoti@yahoo.com>
2022-09-24 19:46:45 +0200

    add categories images

--- bff-backend/src/main/java/fr/univcotedazur/si5ci/microrestaurant/teamf/bffbackend/services/BffService.java
+++ bff-backend/src/main/java/fr/univcotedazur/si5ci/microrestaurant/teamf/bffbackend/services/BffService.java
@@ -31,7 +31,15 @@ public class BffService {
         log.info("Getting all menu categories");
         List<MenuCategory> menuCategories = new ArrayList<>();
         Arrays.stream(Category.values()).forEach(category -> {
-            menuCategories.add(new MenuCategory(category.name()));
+            if (category == Category.STARTER) {
+                menuCategories.add(new MenuCategory(category.name(), "https://pixabay.com/get/g34e6ac45c3e86ff1c50305efe75cfaa640fb3b1eda43696c3fd2d7d09f41f99bf4272fbe6c5d1a175f92b79b4a689fc0_1920.jpg"));
+            }else if(category == Category.MAIN){
+                menuCategories.add(new MenuCategory(category.name(), "https://pixabay.com/get/g51f911f0d264d0f944b2cb1732faf12e331b1a9971730454c15c6467ad5d5a8922ccd968b6a87fd4f32a2f5da0cb8c4d_1920.jpg"));
+            }else if(category == Category.BEVERAGE){
+                menuCategories.add(new MenuCategory(category.name(), "https://pixabay.com/get/ga2b1eed74a8da5b49b271826e94b9585eeb513656da3cfce9664b73805b1bed8200b2140798f242d54ebd96b8aa9520736e416de591d57fce9f31b942137f8f1_1920.jpg"));
+            }else if(category == Category.DESSERT) {
+                menuCategories.add(new MenuCategory(category.name(), "https://pixabay.com/get/g43fb9677d7bc343383f13fd68481de96954f3dd44ce98cb64b6ba069f963322f26478028b1452828124174299b26309b9da20c4056dce7cad3212b8a068f2025_1920.jpg"));
+            }
         });
         return menuCategories;
     }
