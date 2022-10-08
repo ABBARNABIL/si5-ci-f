a3f41bbbf1b9503656a72b8f07a94b60a06e6d46
Nabil Abbar <71392347+ABBARNABIL@users.noreply.github.com>
2022-10-08 13:48:31 +0200

    add quantity modal

--- frontends/bff-front-app/src/components/MenuItemTable.js
+++ frontends/bff-front-app/src/components/MenuItemTable.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import { useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import { useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 function createData(name, pu, qte, pt) {
   return { name, pu, qte, pt };
@@ -17,10 +17,7 @@ function createData(name, pu, qte, pt) {
 //   createData('Ice cream sandwich', 237, 9.0, 37),
 //   createData('Eclair', 262, 16.0, 24),
 
-  
 // ];
-
-
 
 export default function MenuItemTable(props) {
   const [rows, setRows] = React.useState([]);
@@ -28,14 +25,15 @@ export default function MenuItemTable(props) {
   const fillRows = (chooseItems) => {
     let rows = [];
     chooseItems.forEach((value, key) => {
-      rows.push(createData(key, value["pu"], value["nb"], value["pu"] * value["nb"]));
+      rows.push(
+        createData(key, value["pu"], value["nb"], value["pu"] * value["nb"])
+      );
     });
     setRows(rows);
-  }
+  };
 
   useEffect(() => {
     fillRows(props.items);
-    console.log("props.items: " + rows);
   });
 
   console.log("props: " + props.items);
@@ -54,7 +52,7 @@ export default function MenuItemTable(props) {
           {rows.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row.name}
