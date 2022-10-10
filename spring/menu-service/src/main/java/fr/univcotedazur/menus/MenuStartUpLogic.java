7d45922c5cd06e6a1145c89d4a7b52d9b044b2dc
Lidaou Denis Assoti <denisassoti@yahoo.com>
2022-10-10 08:56:37 +0200

    add menus

--- spring/menu-service/src/main/java/fr/univcotedazur/menus/MenuStartUpLogic.java
+++ spring/menu-service/src/main/java/fr/univcotedazur/menus/MenuStartUpLogic.java
@@ -21,14 +21,35 @@ public class MenuStartUpLogic implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        menuItemRepository.save(createMenuItem("Delicious Pizza Regina","pizza",12,STARTER,
+        menuItemRepository.save(createMenuItem("Salade verte italienne","salade italienne",5,STARTER,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/salade.jpg")));
+        menuItemRepository.save(createMenuItem("Salade verte niçoise","salade niçoise",5.3,STARTER,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/salade_nicoise.jpg")));
+        menuItemRepository.save(createMenuItem("Salade verte césar","salade cesar",4.8,STARTER,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/salade_cesar.jpg")));
+        menuItemRepository.save(createMenuItem("Delicious Pizza Regina","pizza",12,MAIN,
                 new URL("https://cdn.pixabay.com/photo/2020/02/27/20/13/cake-4885715_1280.jpg")));
         menuItemRepository.save(createMenuItem("Lasagna al forno","lasagna",16,MAIN,
                 new URL("https://cdn.pixabay.com/photo/2017/02/15/15/17/meal-2069021_1280.jpg")));
+        menuItemRepository.save(createMenuItem("Hamburger fromage doux","burger",8,MAIN,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/burger.jpg")));
+        menuItemRepository.save(createMenuItem("Pasta del fuezo","pasta",9,MAIN,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/pasta.jpg")));
         menuItemRepository.save(createMenuItem("Bottled coke (33cl)","coke",3.5,BEVERAGE,
                 new URL("https://cdn.pixabay.com/photo/2019/11/14/15/47/coke-4626458_1280.jpg")));
+        menuItemRepository.save(createMenuItem("Vin rouge de france","vin rouge",25,BEVERAGE,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/vin%20rouge.jpg")));
+        menuItemRepository.save(createMenuItem("Vin blanc de france","vin blanc",20,BEVERAGE,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/vin%20blanc.jpg")));
         menuItemRepository.save(createMenuItem("Brownie (home made)","brownie",6.5,DESSERT,
                 new URL("https://cdn.pixabay.com/photo/2014/11/28/08/03/brownie-548591_1280.jpg")));
+        menuItemRepository.save(createMenuItem("Tiramissu fait maison","tiramissu",5.5,DESSERT,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/tiramissu.jpg")));
+        menuItemRepository.save(createMenuItem("Glace vanille 3 couches","glace 3",7.5,DESSERT,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/glace.jpg")));
+        menuItemRepository.save(createMenuItem("Tarte citron meringue","tarte citron",8.5,DESSERT,
+                new URL("https://raw.githubusercontent.com/ABBARNABIL/images-ci/main/images/tarte%20citron.jpg")));
+
     }
 
     private static MenuItem createMenuItem(String fullName, String shortName, double price, Category category, URL image) {
